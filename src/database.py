3b868791da7a22d96dbaed8f558f1a8bb44c6fa9
Kalpana <153978607+Kalpanarwt20@users.noreply.github.com>
2026-01-23 11:42:05 +0530

    Add Procfile and fix MySQL env config for Railway

--- src/database.py
+++ src/database.py
@@ -89,16 +89,28 @@
 
 
 import mysql.connector
+import os
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=os.getenv("MYSQLPORT")
+    )
+
+
 
 
 # ---------------- DB CONNECTION ----------------
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="ansu.kalpana1412",
-        database="job_recommender"
-    )
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="ansu.kalpana1412",
+#         database="job_recommender"
+#     )
 
 
 # ---------------- SAVE RESUME (UPDATED) ----------------
